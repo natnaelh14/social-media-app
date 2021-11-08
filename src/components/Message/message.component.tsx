63b3ab9ec4cea75e9d096ef5e4336ffc2760667a
natnaelh14 <natnaelh14@github.com>
2021-11-07 22:11:34 -0500

    created message component

---
+++ src/components/Message/message.component.tsx
@@ -0,0 +1,22 @@
+import { TextContent } from './message.styles';
+
+const Message = () => {
+  return (
+    <div>
+      <TextContent>This is a message</TextContent>
+      <TextContent>This is a message</TextContent>
+      <TextContent>This is a message</TextContent>
+      <TextContent>This is a message</TextContent>
+      <TextContent>This is a message</TextContent>
+      <TextContent>This is a message</TextContent>
+      <TextContent>This is a message</TextContent>
+      <TextContent>This is a message</TextContent>
+      <TextContent>This is a message</TextContent>
+      <TextContent>This is a message</TextContent>
+      <TextContent>This is a message</TextContent>
+      <TextContent>This is a message</TextContent>
+    </div>
+  );
+}
+
+export default Message;
---
+++ src/components/Message/message.styles.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const TextContent = styled.p`
+    text-align: center;
+`;

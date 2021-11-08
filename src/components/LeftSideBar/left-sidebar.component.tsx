2ce09b772a79afba899605bbba8f0a9a38be53c3
natnaelh14 <natnaelh14@github.com>
2021-11-07 22:03:29 -0500

    created left sidebar component

---
+++ src/components/LeftSideBar/left-sidebar.component.tsx
@@ -0,0 +1,225 @@
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HomeIcon from "@mui/icons-material/Home";
+import TagIcon from "@mui/icons-material/Tag";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import {
+  Button,
+  Grid,
+  Hidden,
+  IconButton,
+  Input,
+  Menu,
+  MenuItem,
+  useTheme,
+} from "@mui/material";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import ChatIcon from '@mui/icons-material/Chat';
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import { NavLink } from "react-router-dom";
+
+const LeftSidebar = () => {
+
+  return (
+    <>
+      <Box sx={{ height: "100vh", maxWidth: "100%" }}>
+        <Box textAlign="center">
+          <NavLink
+            to="/"
+            style={{
+              textDecoration: "none",
+              color: "inherit",
+              backgroundColor: "inherit",
+            }}
+          >
+            <img src="https://res.cloudinary.com/doalzf6o2/image/upload/v1635983850/hero-image_yccwx5.png" alt="logo" width="50px" />
+          </NavLink>
+        </Box>
+        <List>
+          <NavLink
+            to="/home/feed"
+            style={{
+              textDecoration: "none",
+              color: "inherit",
+              backgroundColor: "inherit",
+            }}
+          >
+            <ListItem
+              button
+              sx={{
+                borderRadius: "28px",
+                margin: ".5rem 0",
+              }}
+            >
+              <ListItemIcon>
+                <HomeIcon fontSize="medium" color="action" />
+              </ListItemIcon>
+              <Hidden lgDown>
+                <ListItemText
+                  primaryTypographyProps={{
+                    fontSize: "18px",
+                  }}
+                  primary="Home"
+                />
+              </Hidden>
+            </ListItem>
+          </NavLink>
+          <NavLink
+            to={`/home/explore`}
+            style={{
+              textDecoration: "none",
+              color: "inherit",
+              backgroundColor: "inherit",
+            }}
+          >
+            <ListItem
+              button
+              sx={{
+                borderRadius: "28px",
+                margin: ".5rem 0",
+              }}
+            >
+              <ListItemIcon>
+                <TagIcon fontSize="medium" color="action" />
+              </ListItemIcon>
+              <Hidden lgDown>
+                <ListItemText
+                  primaryTypographyProps={{
+                    fontSize: "18px",
+                  }}
+                  primary="Explore"
+                />
+              </Hidden>
+            </ListItem>
+          </NavLink>
+
+          <NavLink
+            to={`/home/notifications`}
+            style={{
+              textDecoration: "none",
+              color: "inherit",
+              backgroundColor: "inherit",
+            }}
+          >
+            <ListItem
+              button
+              sx={{
+                borderRadius: "28px",
+                margin: ".5rem 0",
+              }}
+            >
+              <ListItemIcon>
+                <NotificationsNoneIcon fontSize="medium" color="action" />
+              </ListItemIcon>
+              <Hidden lgDown>
+                <ListItemText
+                  primaryTypographyProps={{
+                    fontSize: "18px",
+                  }}
+                  primary="Notifications"
+                />
+              </Hidden>
+            </ListItem>
+          </NavLink>
+
+          <NavLink
+            to={`/home/messages`}
+            style={{
+              textDecoration: "none",
+              color: "inherit",
+              backgroundColor: "inherit",
+            }}
+          >
+            <ListItem
+              button
+              sx={{
+                borderRadius: "28px",
+                margin: ".5rem 0",
+              }}
+            >
+              <ListItemIcon>
+                <MailOutlineIcon fontSize="medium" color="action" />
+              </ListItemIcon>
+              <Hidden lgDown>
+                <ListItemText
+                  primaryTypographyProps={{
+                    fontSize: "18px",
+                  }}
+                  primary="Messages"
+                />
+              </Hidden>
+            </ListItem>
+          </NavLink>
+
+          <NavLink
+            to={`/home/add-post`}
+            style={{
+              textDecoration: "none",
+              color: "inherit",
+              backgroundColor: "inherit",
+            }}
+          >
+            <ListItem
+              button
+              sx={{
+                borderRadius: "28px",
+                margin: ".5rem 0",
+              }}
+            >
+              <ListItemIcon>
+                <ChatIcon fontSize="medium" color="action" />
+              </ListItemIcon>
+              <Hidden lgDown>
+                <ListItemText
+                  primaryTypographyProps={{
+                    fontSize: "18px",
+                  }}
+                  primary="Chat"
+                />
+              </Hidden>
+            </ListItem>
+
+          </NavLink>
+
+          <NavLink
+            to={`/home/profile`}
+            style={{
+              textDecoration: "none",
+              color: "inherit",
+              backgroundColor: "inherit",
+            }}
+          >
+            <ListItem
+              button
+              sx={{
+                borderRadius: "28px",
+                margin: ".5rem 0",
+              }}
+            >
+              <ListItemIcon>
+                <PersonOutlineIcon fontSize="medium" color="action" />
+              </ListItemIcon>
+              <Hidden lgDown>
+                <ListItemText
+                  primaryTypographyProps={{
+                    fontSize: "18px",
+                  }}
+                  primary="Profile"
+                />
+              </Hidden>
+            </ListItem>
+          </NavLink>
+        </List>
+      </Box>
+    </>
+  );
+}
+
+export default LeftSidebar;

9a45ac3deb0f4b47e7f7b0f9df58d01e43b0a61d
natnaelh14 <natnaelh14@github.com>
2021-11-15 21:32:43 -0500

    able to query post using logged in users

--- src/components/PostList/post-list.component.tsx
+++ src/components/PostList/post-list.component.tsx
@@ -2,29 +2,19 @@ import React, { useEffect } from 'react';
 import Post from '../Post/Post.component';
 import AddPost from '../AddPost/add-post.component';
 import { Fade } from "@mui/material";
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import { useQuery } from '@apollo/client';
-import { QUERY_POSTS } from '../../utils/queries';
+import { useAppSelector } from '../../app/hooks';
 import { CircularProgress, Box } from "@mui/material";
 import { listPosts } from '../../redux/actions/postActions';
 
 const PostList: React.FC = () => {
 
-  const dispatch = useAppDispatch();
-  const currentUser = useAppSelector((state) => state.currentUser);
-  const { user } = currentUser
+  const postList = useAppSelector((state) => state.postList);
+  const { posts, loading } = postList
 
-  const { error, data, loading } = useQuery(QUERY_POSTS, {
-    variables: {
-      user_id: "two"
-    }
-  })
+  // useEffect(() => {
+  //   dispatch(listPosts(data))
 
-  useEffect(() => {
-    dispatch(listPosts(data))
-    console.log('user', user)
-    console.log("data", data)
-  }, [])
+  // }, [dispatch])
 
   return (
 
@@ -32,14 +22,15 @@ const PostList: React.FC = () => {
       <div style={{ border: '1px solid #cdcdcd', padding: '20px', margin: '50px' }}>
         <AddPost />
         <Box>
-          {!data && (
+          {loading && (
             <CircularProgress size={20} color="primary" />
           )}
         </Box>
 
         <Box>
-          {data &&
-            data.posts.map((post: any) => <Post key={post.id} text={post.text} />)}
+          {posts &&
+         (<div>Hello</div>)}
+            {/* posts.map((post: any) => <Post key={post.id} text={post.text} />)} */}
         </Box>
 
       </div>

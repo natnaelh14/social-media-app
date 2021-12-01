24cdc38317586439ead46dbab1188c6e9a4e5c20
natnaelh14 <natnaelh14@github.com>
2021-11-30 22:45:57 -0500

    integrated crypto data to guest profile

--- src/components/CryptoDoughnut/crypto_doughnut.component.tsx
+++ src/components/CryptoDoughnut/crypto_doughnut.component.tsx
@@ -3,7 +3,7 @@ import PieChart, { Legend, Series, Tooltip, Format, Label, Connector, Title, Sub
 import { QUERY_CRYPTOS } from '../../utils/queries';
 import { useQuery } from '@apollo/client';
 
-const CryptoDoughnut: React.FC<{currentUser:string}> = ({currentUser}) => {
+const CryptoDoughnut: React.FC<{ currentUser: string }> = ({ currentUser }) => {
 
     const { loading, data } = useQuery(QUERY_CRYPTOS, {
         variables: {
@@ -24,23 +24,26 @@ const CryptoDoughnut: React.FC<{currentUser:string}> = ({currentUser}) => {
     }
 
     useEffect(() => {
-        (async () => {
-            let cryptoArray: any = []
-            let total = 0
-            for (let i = 0; i < cryptoByUserId.length; i++) {
-                await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${cryptoByUserId[i].crypto_name}`)
-                    .then((res: any) => {
-                        return res.json()
-                    })
-                    .then((res) => {
-                        cryptoArray.push({ region: res[0].name, val: res[0].current_price * cryptoByUserId[i].holding_amount })
-                        total += res[0].current_price * cryptoByUserId[i].holding_amount
-                    })
-            }
-            setCryptoData(cryptoArray)
-            setCryptoTotal(total)
-        })()
-    }, [])
+        if (cryptoByUserId) {
+            (async () => {
+                let cryptoArray: any = []
+                let total = 0
+                for (let i = 0; i < cryptoByUserId.length; i++) {
+                    await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${cryptoByUserId[i].crypto_name}`)
+                        .then((res: any) => {
+                            return res.json()
+                        })
+                        .then((res) => {
+                            cryptoArray.push({ region: res[0].name, val: res[0].current_price * cryptoByUserId[i].holding_amount })
+                            total += res[0].current_price * cryptoByUserId[i].holding_amount
+                        })
+                }
+                setCryptoData(cryptoArray)
+                setCryptoTotal(total)
+            })()
+        }
+
+    }, [cryptoByUserId])
 
     return (
         <>

d336b076a106f6ee0910224d976810dd050641a0
natnaelh14 <natnaelh14@github.com>
2021-12-02 10:45:54 -0500

    integrated progile loading

--- src/pages/styles/crypto_page.styles.tsx
+++ src/pages/styles/crypto_page.styles.tsx
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
+export const CryptoPageContainer = styled.div`
+  width: 75%;
+  margin: 20px;
+  @media (max-width: 1200px) {
+    width: 100%;
+  }
+`;
+
 export const CryptoDataContainer = styled.div`
   display: flex;
   flex-direction: row;

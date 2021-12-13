de222cf259ed9ab7c98ebf2b1d0905d9e5118a9f
natnaelh14 <natnaelh14@github.com>
2021-12-13 11:50:33 -0500

    created routing error and new message refresh

--- src/components/AddPost/add-post.component.tsx
+++ src/components/AddPost/add-post.component.tsx
@@ -1,13 +1,11 @@
 
-import React, { useState, MouseEvent } from "react";
+import React, { useState } from "react";
 import { Button, Grid, Input, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import { Box } from "@mui/system";
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { useAppSelector } from '../../app/hooks';
 import { useQuery, useMutation } from '@apollo/client';
 import { ADD_POST, UPDATE_USER_PROFILE } from "../../utils/mutations";
 import { userProps } from '../../index.types';
-import { QUERY_POSTS_BY_FOLLOWING } from '../../utils/queries';
-import { listPostsByFollowing } from '../../redux/actions/postActions';
 import AddPostLoading from './add_post_loading.component';
 import noAvatar from '../../img/no-avatar.png';
 import Avatar from '@material-ui/core/Avatar';
@@ -17,14 +15,12 @@ type postProps = {
 }
 
 const AddPost = ({ refetchPosts }: postProps) => {
-
-  const dispatch = useAppDispatch()
   const currentUser = useAppSelector((state) => state.currentUser);
   const { user, loading: userLoading } = currentUser
   const userInfo: userProps = user
 
   const [postText, setPostText] = useState("");
-  const [mood, setMood] = useState<string>(userInfo.status)
+  const [mood, setMood] = useState<string>(userInfo?.status)
   const [addPost, { }] = useMutation(ADD_POST);
   const [updateProfile, { }] = useMutation(UPDATE_USER_PROFILE)
 
